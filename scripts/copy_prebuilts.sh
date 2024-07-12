8eeb13f74d43c87234b29ac032939c2736b1fe2c
git-bruh <prathamIN@proton.me>
2024-07-12 18:05:43 +0530

    scripts: copy_prebuilts: split debug info outside build tree

--- scripts/copy_prebuilts.sh
+++ scripts/copy_prebuilts.sh
@@ -10,17 +10,6 @@ rm -f \
 LIBCWEBRTC_PATH="$HOME/libwebrtc/src/out/Release"
 LIBMOBILECORE_PATH="$HOME/native/mobile-core/shared/build/bin/linuxX64/releaseShared"
 
-copy_and_split() {
-	base="$(basename "$2")"
-
-	cp "$2" "$1/"
-
-	objcopy --only-keep-debug "$1/$base" "$1/$base.dbg"
-	objcopy --strip-unneeded "$1/$base"
-	objcopy --add-gnu-debuglink="$1/$base.dbg" "$1/$base"
-}
-
 cp "$LIBMOBILECORE_PATH/libmobilecore_api.h" third_party/libmobilecore
-
-copy_and_split third_party/libcwebrtc "$LIBCWEBRTC_PATH/libcwebrtc.so"
-copy_and_split third_party/libmobilecore "$LIBMOBILECORE_PATH/libmobilecore.so"
+cp "$LIBMOBILECORE_PATH/libmobilecore.so" third_party/libmobilecore
+cp "$LIBCWEBRTC_PATH/libcwebrtc.so" third_party/libcwebrtc
